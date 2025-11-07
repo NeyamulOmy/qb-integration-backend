8a8fe581674aeed01288b9dc63a22a935bc75f94
NeyamulOmy <omy@eagle3dstreaming.com>
2025-11-07 20:31:58 +0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const { app } = require("./app");
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`API running at http://localhost:${PORT}`));
